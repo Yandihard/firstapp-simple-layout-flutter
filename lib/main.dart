7a4ba6aa639ef2a04de5b0f6194c8a9a04c94e2e
Yandihard <yandihard95@gmail.com>
2022-05-18 16:42:29 +0700

    menambahkan mainscreen dan model

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:firstapp/detail_screen.dart';
+import 'package:firstapp/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Wisata Bandung',
       theme: ThemeData(),
-      home: const DetailScreen(),
+      home: const MainScreen(),
     );
   }
 }
